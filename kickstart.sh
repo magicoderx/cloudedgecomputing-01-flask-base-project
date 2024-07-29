5e8bc59a4521e7fc73b3d78879efb45b0a2ae384
DevelopMagic <pesole99@live.it>
2024-07-29 12:18:16 +0200

    Add hostname for db instead IP

--- kickstart.sh
+++ kickstart.sh
@@ -4,8 +4,9 @@ export FLASK_APP=app.py
 flask db init
 flask db migrate
 flask db upgrade
-exec gunicorn -w 4 -b 0.0.0.0:8080 wsgi:app
 
 if [ "$RUN_TESTS" = "true" ]; then
     exec python test_http.py  
+else
+    exec gunicorn -w 4 -b 0.0.0.0:8080 wsgi:app
 fi

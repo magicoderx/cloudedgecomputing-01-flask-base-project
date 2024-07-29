7c8a0cbaabb693ec609e2e3de2cb9fe277123ec6
DevelopMagic <pesole99@live.it>
2024-07-29 14:01:46 +0200

    Fixed missing password PT.2

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,10 @@ SERVER="root@3.74.7.83"
 DEPLOY_PATH="/data/proj/"
 
 # Sync file with production server
+printf "\n\n\nPWD: %s\n\n\n" $SSH_PSWD
+echo $SERVER
+echo $DEPLOY_PATH
+printf "\n\n\nSecrets command: %s\n\n\n" $(secrets.SSH_PSWD)
 sshpass -p "$SSH_PSWD" rsync -e 'ssh -o StrictHostKeyChecking=no -p 3224' -avz --delete --exclude '.git*' . $SERVER:$DEPLOY_PATH
 
 # Post-deployment commands

11148f39a72fb62bb4314f0d7351e648e6b6961b
DevelopMagic <pesole99@live.it>
2024-07-29 13:42:01 +0200

    Fix fingerprint issue for ssh

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ SERVER="root@3.74.7.83"
 DEPLOY_PATH="/data/proj/"
 
 # Sync file with production server
-sshpass -p "$SSH_PASSWORD" rsync -e 'ssh -p 3224' -avz --delete --exclude '.git*' . $SERVER:$DEPLOY_PATH
+sshpass -p "$SSH_PASSWORD" rsync -e 'ssh -o StrictHostKeyChecking=no -p 3224' -avz --delete --exclude '.git*' . $SERVER:$DEPLOY_PATH
 
 # Post-deployment commands
-sshpass -p "$SSH_PASSWORD" ssh -p 3224 -tt $SERVER << 'ENDSSH'
+sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no -p 3224 -tt $SERVER << 'ENDSSH'
 cd /data/proj/
 docker-compose down
 docker-compose up --build -d

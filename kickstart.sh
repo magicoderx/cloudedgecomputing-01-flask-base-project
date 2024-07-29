abc1af822bac50dd7737db194c846b048c390a7d
DevelopMagic <pesole99@live.it>
2024-07-29 11:33:44 +0200

    Adding temporary docker image for cd/ci

--- kickstart.sh
+++ kickstart.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-export FLASK_APP=app.py
-flask db init
-flask db migrate
-flask db upgrade
-exec gunicorn -w 4 -b 0.0.0.0:8080 wsgi:app
+
+if [ "$RUN_TESTS" = "true" ]; then
+    exec python test_http.py
+else
+    export FLASK_APP=app.py
+    flask db init
+    flask db migrate
+    flask db upgrade
+    exec gunicorn -w 4 -b 0.0.0.0:8080 wsgi:app
+fi
